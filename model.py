1b45d1e342a1f43835acdf4bb4d5da7f304cd1ac
JeongJaecheol <jcjong12110@gmail.com>
2018-07-04 17:44:00 +0900

    fix flownet class

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 # tensorflow keras
+import tensorflow as tf
 from tensorflow.contrib.keras.api.keras.optimizers import RMSprop, Adam
 from tensorflow.contrib.keras.api.keras.models import Model, Sequential
 from tensorflow.contrib.keras.api.keras.layers import Input , Activation
@@ -9,68 +10,83 @@ from tensorflow.contrib.keras.api.keras.backend import resize_images
 
 # class FlowNetCorr(object):
 
-class FlowNetSimple(object):
+class FlowNet(object):
 
     def __init__(self, img_height, img_width, img_depth,  learning_rate):
-        self.img_height = img_height
-        self.img_width = img_width
         self.img_depth = img_depth
         self.learning_rate = learning_rate
 
-    def conv_layers(self):
-        seq = Sequential()
+        self.img_height = self.proximateMultiple(img_height)
+        self.img_width = self.proximateMultiple(img_width)
+
+        print('input image resized by (height = %s,' %self.img_height, 'width = %s)' %self.img_width)
+
+    def proximateMultiple(self, value, multiple = 64):
+        n = 1
+        condition = False
+        diff_before = value
+        diff_after = 0
+
+        while not condition:
+            diff_after = abs(multiple * n - value)
+
+            if diff_after < diff_before:
+                diff_before = diff_after
+            else:
+                condition = True
+                return multiple * (n - 1)
+            n += 1
+    
+    def FlowNetSimple(self, input): 
         ''' convolution layers : Conv1 - Conv2 - Conv3 - Conv3_1 - Conv4 - Conv4_1 - Conv5 - Conv5_1 - Conv6 '''
-        seq.add(Conv2D(64, (7, 7), input_shape=(self.img_height, self.img_width, self.img_depth), padding='same', name='Conv1'))
-        seq.add(MaxPooling2D(pool_size=(2, 2)))
-        seq.add(Conv2D(128, (5, 5), padding='same', name='Conv2'))
-        Conv2 = seq.add(MaxPooling2D(pool_size=(2, 2)))
-        seq.add(Conv2D(256, (5, 5), padding='same', name='Conv3'))
-        seq.add(MaxPooling2D(pool_size=(2, 2)))
-        Conv3_1 = seq.add(Conv2D(256, (3, 3), padding='same', name='Conv3_1'))
-        seq.add(Conv2D(512, (3, 3), padding='same', name='Conv4'))
-        seq.add(MaxPooling2D(pool_size=(2, 2)))
-        Conv4_1 = seq.add(Conv2D(512, (3, 3), padding='same', name='Conv4_1'))
-        seq.add(Conv2D(512, (3, 3), padding='same', name='Conv5'))
-        seq.add(MaxPooling2D(pool_size=(2, 2)))
-        Conv5_1 = seq.add(Conv2D(512, (3, 3), padding='same', name='Conv5_1'))
-        seq.add(Conv2D(1024, (3, 3), padding='same', name='Conv6'))
-        seq.add(MaxPooling2D(pool_size=(2, 2)))
-
-        return seq, Conv5_1, Conv4_1, Conv3_1, Conv2
-
-    def deconv_layers(self, Conv5_1, Conv4_1, Conv3_1, Conv2): 
-        seq = Sequential()
+        Conv1 = Conv2D(64, (7, 7), (2, 2), padding='same', activation='relu', name='Conv1')(input)
+        Conv2 = Conv2D(128, (5, 5), (2, 2), padding='same', activation='relu', name='Conv2')(Conv1)
+        Conv3 = Conv2D(256, (5, 5), (2, 2), padding='same', activation='relu', name='Conv3')(Conv2)
+        Conv3_1 = Conv2D(256, (3, 3), padding='same', activation='relu', name='Conv3_1')(Conv3)
+        Conv4 = Conv2D(512, (3, 3), (2, 2), padding='same', activation='relu', name='Conv4')(Conv3_1)
+        Conv4_1 = Conv2D(512, (3, 3), padding='same', activation='relu', name='Conv4_1')(Conv4)
+        Conv5 = Conv2D(512, (3, 3), (2, 2), padding='same', activation='relu', name='Conv5')(Conv4_1)
+        Conv5_1 = Conv2D(512, (3, 3), padding='same', activation='relu', name='Conv5_1')(Conv5)
+        Conv6 = Conv2D(1024, (3, 3), (2, 2), padding='same', activation='relu', name='Conv6')(Conv5_1)
+        
         ''' deconvolution layers : deconv5(flow5) - deconv4(flow4) - deconv3(flow3) - deconv2 - prediction '''
-        deconv5 = seq.add(Conv2DTranspose(512, (5, 5), padding='same', name='deconv5'))
-        seq.add(concatenate([deconv5, Conv5_1]))
-        flow5 = seq.add(Conv2D(1, (5, 5), padding='same', name='flow5'))
+        #deconv5 = conv2d_transpose(Conv6, (3, 3), tf.shape(Conv5_1), (2, 2), padding='same')
+        deconv5 = Conv2DTranspose(512, (3, 3), (2, 2), padding='same', activation='relu', name='deconv5')(Conv6)
+        concat1 = concatenate([deconv5, Conv5_1], axis = 3, name='concat1')
 
-        deconv4 = seq.add(Conv2DTranspose(512, (5, 5), padding='same', name='deconv4'))
-        seq.add(concatenate([deconv4, Conv4_1, flow5]))
-        flow4 = seq.add(Conv2D(1, (5, 5), padding='same', name='flow4'))
+        flow5 = Conv2D(2, (3, 3), padding='same', name='flow5')(concat1)
+        flow5_up = Conv2DTranspose(2, (3, 3), (2, 2), padding='same', name='flow5_up')(flow5)
 
-        deconv3 = seq.add(Conv2DTranspose(512, (5, 5), padding='same', name='deconv3'))
-        seq.add(concatenate([deconv3, Conv3_1, flow4]))
-        flow3 = seq.add(Conv2D(1, (5, 5), padding='same', name='flow3'))
+        deconv4 = Conv2DTranspose(256, (3, 3), (2, 2), padding='same', activation='relu', name='deconv4')(concat1)
+        concat2 = concatenate([deconv4, Conv4_1, flow5_up], axis = 3, name='concat2')
 
-        deconv2 = seq.add(Conv2DTranspose(512, (5, 5), padding='same', name='deconv2'))
-        seq.add(concatenate([deconv2, Conv2, flow3]))
-        prediction = seq.add(Conv2D(1, (5, 5), padding='same', name='prediction'))
+        flow4 = Conv2D(1, (3, 3), padding='same', name='flow4')(concat2)
+        flow4_up = Conv2DTranspose(2, (3, 3), (2, 2), padding='same', name='flow4_up')(flow4)
+
+        deconv3 = Conv2DTranspose(128, (5, 5), (2, 2), padding='same', activation='relu', name='deconv3')(concat2)
+        concat3 = concatenate([deconv3, Conv3_1, flow4_up], axis = 3, name='concat3')
+
+        flow3 = Conv2D(1, (3, 3), padding='same', name='flow3')(concat3)
+        flow3_up = Conv2DTranspose(2, (3, 3), (2, 2), padding='same', name='flow3_up')(flow3)
+
+        deconv2 = Conv2DTranspose(64, (5, 5), (2, 2), padding='same', activation='relu', name='deconv2')(concat3)
+        concat4 = concatenate([deconv2, Conv2, flow3_up], axis = 3, name='concat4')
+        prediction = Conv2D(1, (3, 3), padding='same', name='prediction')(concat4)
 
         return prediction
 
-    def inference(self):
+    def inference(self, mode = 'simple'):
 
-        stack2image = Input(shape=(self.img_height, self.img_width, self.img_width * 2), name='stack2image')
-        encoded, Conv5_1, Conv4_1, Conv3_1, Conv2 = self.conv_layers()(stack2image)
-        prediction = self.deconv_layers(Conv5_1, Conv4_1, Conv3_1, Conv2)(encoded)
+        if mode == 'simple':
+            simple_input = Input(shape=(self.img_height, self.img_width, self.img_depth * 2), name='simple_input')
+            prediction = self.FlowNetSimple(simple_input)
 
-        FlowNetS = Model(inputs = [stack2image], outputs = [prediction])
-        opt = Adam(lr=self.learning_rate)
-        FlowNetS.compile(optimizer=opt, loss='mae')
-        FlowNetS.summary() 
-        
-        return FlowNetS
+            FlowNet = Model(inputs = [simple_input], outputs = [prediction])
+            opt = Adam(lr=self.learning_rate)
+            FlowNet.compile(optimizer=opt, loss='mae')
+            FlowNet.summary() 
+            
+            return FlowNet
 
 class EPINet(object):
 
@@ -86,15 +102,15 @@ class EPINet(object):
         seq = Sequential()
         ''' Multi-Stream layer : Conv - Relu - Conv - BN - Relu  '''
 
-        # seq.add(Reshape((input_dim1,input_dim12,input_dim3),input_shape=(input_dim1, input_dim2, input_dim3,1)))
+        # (Reshape((input_dim1,input_dim12,input_dim3),input_shape=(input_dim1, input_dim2, input_dim3,1)))
         for i in range(3):
-            seq.add(Conv2D(int(filt_num),(2,2),input_shape=(input_dim1, input_dim2, input_dim3), padding='valid', name='S1_c1%d' %(i) ))
-            seq.add(Activation('relu', name='S1_relu1%d' %(i))) 
-            seq.add(Conv2D(int(filt_num),(2,2), padding='valid', name='S1_c2%d' %(i) )) 
-            seq.add(BatchNormalization(axis=-1, name='S1_BN%d' % (i)))
-            seq.add(Activation('relu', name='S1_relu2%d' %(i))) 
+            (Conv2D(int(filt_num),(2,2),input_shape=(input_dim1, input_dim2, input_dim3), padding='valid', name='S1_c1%d' %(i) ))
+            (Activation('relu', name='S1_relu1%d' %(i))) 
+            (Conv2D(int(filt_num),(2,2), padding='valid', name='S1_c2%d' %(i) )) 
+            (BatchNormalization(axis=-1, name='S1_BN%d' % (i)))
+            (Activation('relu', name='S1_relu2%d' %(i))) 
 
-        seq.add(Reshape((input_dim1-6,input_dim2-6,int(filt_num))))
+        (Reshape((input_dim1-6,input_dim2-6,int(filt_num))))
 
         return seq  
 
@@ -104,11 +120,11 @@ class EPINet(object):
         seq = Sequential()
         
         for i in range(conv_depth):
-            seq.add(Conv2D(filt_num,(2,2), padding='valid',input_shape=(input_dim1, input_dim2, input_dim3), name='S2_c1%d' % (i) ))
-            seq.add(Activation('relu', name='S2_relu1%d' %(i))) 
-            seq.add(Conv2D(filt_num,(2,2), padding='valid', name='S2_c2%d' % (i))) 
-            seq.add(BatchNormalization(axis=-1, name='S2_BN%d' % (i)))
-            seq.add(Activation('relu', name='S2_relu2%d' %(i)))
+            (Conv2D(filt_num,(2,2), padding='valid',input_shape=(input_dim1, input_dim2, input_dim3), name='S2_c1%d' % (i) ))
+            (Activation('relu', name='S2_relu1%d' %(i))) 
+            (Conv2D(filt_num,(2,2), padding='valid', name='S2_c2%d' % (i))) 
+            (BatchNormalization(axis=-1, name='S2_BN%d' % (i)))
+            (Activation('relu', name='S2_relu2%d' %(i)))
             
         return seq     
 
@@ -118,10 +134,10 @@ class EPINet(object):
         seq = Sequential()
         
         for i in range(1):
-            seq.add(Conv2D(filt_num,(2,2), padding='valid',input_shape=(input_dim1, input_dim2, input_dim3), name='S3_c1%d' %(i) )) # pow(25/23,2)*12*(maybe7?) 43 3
-            seq.add(Activation('relu', name='S3_relu1%d' %(i)))
+            (Conv2D(filt_num,(2,2), padding='valid',input_shape=(input_dim1, input_dim2, input_dim3), name='S3_c1%d' %(i) )) # pow(25/23,2)*12*(maybe7?) 43 3
+            (Activation('relu', name='S3_relu1%d' %(i)))
             
-        seq.add(Conv2D(1,(2,2), padding='valid', name='S3_last')) 
+        (Conv2D(1,(2,2), padding='valid', name='S3_last')) 
 
         return seq 
 
